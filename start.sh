4694fcd2af10f35d1763afdf40f23749c26d10fc
RAJESH S <87179096+rajesh-ezee@users.noreply.github.com>
2024-05-25 18:45:41 +0530

    start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
+  git clone https://github.com/rajesh-ezee/Pepper-Music /Pepper-Music
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/subinps/VCPlayerBot -b $BRANCH /VCPlayerBot
+  git clone https://github.com/rajesh-ezee/Pepper-Music -b $BRANCH /Pepper-Music
 fi
-cd /VCPlayerBot
+cd /Pepper-Music
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
